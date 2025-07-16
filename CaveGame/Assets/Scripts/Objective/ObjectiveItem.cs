b95be7676d004f9ceb624bdb31efd103f1e73623
JustinMelore <melorejustin@gmail.com>
2025-07-15 19:45:59 -0600

    Made the player able to interact with the objective

--- CaveGame/Assets/Scripts/Objective/ObjectiveItem.cs
+++ CaveGame/Assets/Scripts/Objective/ObjectiveItem.cs
@@ -7,6 +7,17 @@ public class ObjectiveItem : MonoBehaviour
     public static event Action<ObjectiveItem> OnObjectiveRangeExit;
 
     [SerializeField] private SphereCollider detectionRange;
+    [SerializeField] private InteractionRange interactionRange;
+
+    private void Awake()
+    {
+        PlayerController.OnInteractWithObject += InteractWithObjective;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnInteractWithObject -= InteractWithObjective;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,5 +32,19 @@ public class ObjectiveItem : MonoBehaviour
     public float GetObjectiveRange()
     {
         return detectionRange.radius;
+    }
+
+    private void InteractWithObjective(InteractionRange interactable)
+    {
+        if(interactable == interactionRange)
+        {
+            detectionRange.gameObject.SetActive(false);
+            PlayerController.OnInteractWithObject -= InteractWithObjective;
+            OnObjectiveRangeExit?.Invoke(this);
+            //Will be replaced with a different visual indicator in the future, like an interact animation
+            GetComponentInChildren<Renderer>().material.color = new Color(0f, 1f, 0f);
+            Debug.Log($"Objective collected");
+            this.enabled = false;
+        }
     }
 }
--- CaveGame/Assets/Scripts/Player/PlayerController.cs
+++ CaveGame/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
@@ -24,10 +25,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField][UnityEngine.Range(0, 1)] private float radioStaticMaxVolume;
     [SerializeField][UnityEngine.Range(0, 1)] private float radioObjectiveMaxVolume;
 
+    [Header("Interaction Settings")]
+    [SerializeField] [UnityEngine.Range(-1, 1)] private float minimumInteractableCloseness;
+
+    public static event Action<InteractionRange> OnLookAtInteractable;
+    public static event Action<InteractionRange> OnLookAwayFromInteractable;
+    public static event Action<InteractionRange> OnInteractWithObject;
+
     private Vector3 playerVelocity;
     private Vector3 playerRotation;
     private bool isRunning = false;
     private bool isTuning = false;
+    private InteractionRange interactable;
+    private bool canInteract;
 
     //Made a hash set to easily add and remove objectives
     private HashSet<ObjectiveItem> objectivesInRange;
@@ -39,6 +49,8 @@ public class PlayerController : MonoBehaviour
         objectivesInRange = new HashSet<ObjectiveItem>();
         ObjectiveItem.OnObjeciveRangeEnter += AddObjective;
         ObjectiveItem.OnObjectiveRangeExit += RemoveObjective;
+        InteractionRange.OnInteractRangeEnter += RegisterInteractable;
+        InteractionRange.OnInteractRangeExit += RemoveInteractable;
         radioStaticSource.volume = 0f;
         radioObjectiveSoundSource.volume = 0f;
         Cursor.lockState = CursorLockMode.Locked;
@@ -51,7 +63,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnMove(InputValue inputValue)
     {
-        Vector3 inputVector = inputValue.Get<Vector3>();
+        Vector3 inputVector = Vector3.Normalize(inputValue.Get<Vector3>());
         playerVelocity.x = inputVector.x;
         playerVelocity.z = inputVector.z;
     }
@@ -71,13 +83,22 @@ public class PlayerController : MonoBehaviour
         {
 
             radioStaticSource.volume = radioStaticMaxVolume;
-            //Debug.Log("Tuning radio");
+            Debug.Log("Tuning radio");
         }
         else
         {
             radioStaticSource.volume = 0f;
             radioObjectiveSoundSource.volume = 0f;
-            //Debug.Log("Stopped tuning radio");
+            Debug.Log("Stopped tuning radio");
+        }
+    }
+
+    private void OnInteract(InputValue inputValue)
+    {
+        if (canInteract && interactable != null)
+        {
+            OnInteractWithObject?.Invoke(interactable);
+            Debug.Log($"Interacted with {interactable}");
         }
     }
 
@@ -86,6 +107,7 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
         if (isTuning) ScanForObjectives();
+        if (interactable != null) CheckForInteraction();
     }
 
     private void MovePlayer()
@@ -98,6 +120,25 @@ public class PlayerController : MonoBehaviour
         movement.y = playerVelocity.y;
         playerCamera.transform.localRotation = Quaternion.Euler(playerRotation.x, 0f, 0f);
         characterController.Move(movement * Time.deltaTime);
+    }
+
+    private void CheckForInteraction()
+    {
+        float interactableLookCloseness = CalculateLookCloseness(interactable.gameObject);
+        if(interactableLookCloseness >= minimumInteractableCloseness)
+        {
+            if (!canInteract)
+            {
+                OnLookAtInteractable?.Invoke(interactable);
+                Debug.Log($"Now looking at {interactable}");
+            }
+            canInteract = true;
+        } else if(canInteract)
+        {
+            OnLookAwayFromInteractable?.Invoke(interactable);
+            canInteract = false;
+            Debug.Log($"Looked away from {interactable}");
+        }
     }
 
     public bool IsMoving()
@@ -119,13 +160,13 @@ public class PlayerController : MonoBehaviour
     private void AddObjective(ObjectiveItem objective)
     {
         objectivesInRange.Add(objective);
-        //Debug.Log($"Objective in range; {objectivesInRange.Count} objectives in range");
+        Debug.Log($"Objective in range; {objectivesInRange.Count} objectives in range");
     }
 
     private void RemoveObjective(ObjectiveItem objective)
     {
         objectivesInRange.Remove(objective);
-        //Debug.Log($"Objective out of range; {objectivesInRange.Count} objectives in range");
+        Debug.Log($"Objective out of range; {objectivesInRange.Count} objectives in range");
     }
 
 #nullable enable
@@ -143,9 +184,10 @@ public class PlayerController : MonoBehaviour
         float distancePercentage = 0f;
         foreach(ObjectiveItem objective in objectivesInRange)
         {
-            Vector3 directionToObjective = objective.transform.position - transform.position;
-            directionToObjective.Normalize();
-            float currentObjectiveDotProduct = Vector3.Dot(directionToObjective, transform.forward);
+            //Vector3 directionToObjective = objective.transform.position - transform.position;
+            //directionToObjective.Normalize();
+            //float currentObjectiveDotProduct = Vector3.Dot(directionToObjective, transform.forward);
+            float currentObjectiveDotProduct = CalculateLookCloseness(objective.gameObject);
             float distanceToObjective = Vector3.Distance(objective.transform.position, transform.position);
             distancePercentage = (objective.GetObjectiveRange() - distanceToObjective - 1) / (objective.GetObjectiveRange() - 1);
             minimumCloseness = minimumObjectiveCloseness - distancePercentage * minimumObjectiveCloseness;
@@ -158,8 +200,8 @@ public class PlayerController : MonoBehaviour
         if(mostDirectObjective != null) {
             float closenessRange = 1f - minimumCloseness;
             mostDirectDot = Mathf.Round(mostDirectDot * 100) / 100 - minimumCloseness;
-            //Debug.Log($"Closeness range: {closenessRange}");
-            //Debug.Log($"Closeness: {mostDirectDot}");
+            Debug.Log($"Closeness range: {closenessRange}");
+            Debug.Log($"Closeness: {mostDirectDot}");
             float objectiveVolumePercentage = mostDirectDot / closenessRange;
             radioStaticSource.volume = radioStaticMaxVolume - radioStaticMaxVolume * objectiveVolumePercentage;
             //radioObjectiveSoundSource.volume = radioObjectiveMaxVolume * objectiveVolumePercentage;
@@ -169,5 +211,24 @@ public class PlayerController : MonoBehaviour
             radioStaticSource.volume = radioStaticMaxVolume;
             radioObjectiveSoundSource.volume = 0f;
         }
+    }
+
+    private float CalculateLookCloseness(GameObject other)
+    {
+        Vector3 directionToObject = other.transform.position - transform.position;
+        directionToObject.Normalize();
+        return Vector3.Dot(directionToObject, transform.forward);
+    }
+
+    private void RegisterInteractable(InteractionRange interactable)
+    {
+        this.interactable = interactable;
+        Debug.Log($"Entered interaction range of {interactable}");
+    }
+
+    private void RemoveInteractable(InteractionRange interactable)
+    {
+        this.interactable = null;
+        Debug.Log($"Exited interaction range of {interactable}");
     }
 }
