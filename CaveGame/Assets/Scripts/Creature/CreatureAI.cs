5e0493bfdc7da9a34927936ca49c2c85814e9a91
PeterBallas <159381135+PeterBallas@users.noreply.github.com>
2025-07-16 21:56:54 -0400

    Made AI file
    Just made the AI file before having to go help my parents with something

---
+++ CaveGame/Assets/Scripts/Creature/CreatureAI.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CreatureAI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
